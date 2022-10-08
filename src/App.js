54f53cce84eba53cf0a8e101a02695712a135b39
Prayush Dave <prayushdave@gmail.com>
2022-10-08 22:00:21 +0530

    Complete Row Design

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HomeScreen from './HomeScreen';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <HomeScreen/>
     </div>
   );
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,16 +2,25 @@ import React from 'react'
 import Banner from './Banner'
 import "./HomeScreen.css"
 import Nav from './Nav'
+import requests from './Requests'
+import Row from './Row'
 
 const HomeScreen = () => {
   return (
     <div className='homeScreen'>
-        {/* NavBar */}
+        
         <Nav/>
         
         <Banner/>
 
-        {/* Row */}
+        <Row title='Netflix Originals' fetchUrl={requests.fetchNetflixOriginals} isLargeRow ></Row>
+        <Row title='Trending Now' fetchUrl={requests.fetchTrending}></Row>
+        <Row title='Top Rated' fetchUrl={requests.fetchTopRated}></Row>
+        <Row title='Action' fetchUrl={requests.fetchActionMovies}></Row>
+        <Row title='Comedy' fetchUrl={requests.fetchComedyMovies}></Row>
+        <Row title='Romance' fetchUrl={requests.fetchRomanceMovies}></Row>
+        <Row title='Horror' fetchUrl={requests.fetchHorrorMovies}></Row>
+        <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries}></Row>
     </div>
   )
 }
