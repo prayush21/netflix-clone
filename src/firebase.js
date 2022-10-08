1b546ea360d9affb26e8128e03b6b62d74b18de0
Prayush Dave <prayushdave@gmail.com>
2022-10-08 14:02:50 +0530

    Complete React and Firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDnOYUt62fGfPXu-eBbt3SaTEzuRDKJS10",
+    authDomain: "netflix-clone-fe8ae.firebaseapp.com",
+    projectId: "netflix-clone-fe8ae",
+    storageBucket: "netflix-clone-fe8ae.appspot.com",
+    messagingSenderId: "52883317022",
+    appId: "1:52883317022:web:4d36975f93ce5d313d290f"
+  };
