81186baa90fee22d3bdbd21efdab42bfe43bdced
Prayush Dave <prayushdave@gmail.com>
2022-10-09 22:55:18 +0530

    Profile Page Basic Setup Complete

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import "./Nav.css"
 
 const Nav = () => {
     const [show, handleShow] = useState(false);
+    const navigate = useNavigate();
 
     const handleNavbarTransition = () => {
         if(window.scrollY > 100){
@@ -21,8 +23,8 @@ const Nav = () => {
     <div className={`nav ${show && 'nav__black'}`}>
         <div className="nav__contents">
             
-        <img className='nav__logo' src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt="netflix-logo" />
-        <img className='nav__avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="avatar" />
+        <img className='nav__logo' onClick={() => navigate('/')} src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt="netflix-logo" />
+        <img className='nav__avatar' onClick={() => navigate('/profile')} src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="avatar" />
         </div>
     </div>
   )
