e03f5d5afaf1e928bcd123d3650452f2d920caeb
Prayush Dave <prayushdave@gmail.com>
2022-10-09 15:37:21 +0530

    Complete firebase auth

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import Login from "./screens/LoginScreen";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, userAuth => {
+      if(userAuth){
+        console.log('logged in: userAuth', userAuth);
+      } else {
+        //logged out
+      }
+    })
+
+    return unsubscribe;
+  }, []);
   
   return (
     <div className="app">
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,29 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./SignUp.css"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useRef } from "react";
+import { auth } from "./firebase";
+import "./SignUp.css";
 
 const SignUp = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
-    const register = (e) => {
-        e.preventDefault();
-        alert('reg')
-    }
+  const register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+      .then((authUser) => {
+        console.log("authUser", authUser);
+        alert("Registered");
+      })
+      .catch((err) => alert(err.message));
+  };
 
-    const signInFunc = (e) => {
-        e.preventDefault();
-        alert('signin')
+  const signInFunc = (e) => {
+    e.preventDefault();
 
-    }
+    signInWithEmailAndPassword(auth, emailRef.current.value,
+        passwordRef.current.value) .then((authUser) => {
+            console.log("authUser", authUser);
+            alert("signin");
+          })
+          .catch((err) => alert(err.message));
+
+    
+  };
 
   return (
-    <div className='signUp'>
-        <h1>Sign In</h1>
-        <input type="email"  placeholder='Email'/>
-        <input type="password"  placeholder='Password'/>
-        <button type='submit' onClick={signInFunc}>Sign In</button>
-        <h3>New to Netflix? <a onClick={register}>Sign Up Now!</a></h3>
+    <div className="signUp">
+      <h1>Sign In</h1>
+      <input ref={emailRef} type="email" placeholder="Email" />
+      <input ref={passwordRef} type="password" placeholder="Password" />
+      <button type="submit" onClick={signInFunc}>
+        Sign In
+      </button>
+      <h3>
+        New to Netflix? <span onClick={register}>Sign Up Now!</span>
+      </h3>
     </div>
-  )
-}
- 
-export default SignUp
+  );
+};
+
+export default SignUp;
